2626b99c92ac57e5e0766423eb18edb6f1e7970e
Clayton Dewey <clay@dryadmedia.com>
2021-05-13 13:39:03 -0500

    Added submittal form

--- app/controllers/note.controller.js
+++ app/controllers/note.controller.js
@@ -1,7 +1,7 @@
 const Note = require("../models/note.model.js");
 
 // Create and Save a new Note
-exports.create = (req, res) => {
+exports.create = async (req, res) => {
   // Validate request
   if (!req.body.content) {
     return res.status(400).send({
@@ -16,16 +16,14 @@ exports.create = (req, res) => {
   });
 
   // Save Note in the database
-  note
-    .save()
-    .then((data) => {
-      res.send(data);
-    })
-    .catch((err) => {
-      res.status(500).send({
-        message: err.message || "Some error occurred while creating the Note.",
-      });
+  try {
+    await note.save();
+    res.redirect("/notes");
+  } catch (err) {
+    res.status(500).send({
+      message: err.message || "Some error occurred while creating the Note.",
     });
+  }
 };
 
 // Retrieve and return all notes from the database.
